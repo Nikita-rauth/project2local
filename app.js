629f2b8e6e3269345ae41659d5115454a59ddd4e
Nikita Rauth <nikitarauth13@gmail.com>
2025-06-20 16:51:04 +0530

    @pb@

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 console.log("jai shri krishn");
 console.log("radhe radhe");
 console.log("Jai maa santoshi");
-<<<<<<< HEAD
 let p=document.querySelector("p");
-=======
 let btn=document.querySelector("button");
->>>>>>> main
